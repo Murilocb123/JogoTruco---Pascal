a819930d62ca7745e63e43919e7a5e859be16697
Murilocb123 <murilocb1276@gmail.com>
2023-04-14 20:17:49 -0300

    Conversa dia 14/04

--- views.pas
+++ views.pas
@@ -2,6 +2,7 @@ unit views;
 interface
     procedure views_menu_main();
     procedure views_authors();
+    procedure views_menu_jogada();
 
 implementation
     procedure views_menu_main();
@@ -18,6 +19,43 @@ implementation
     begin
         writeln('Murilo Lindo, Alison feio que doi e Gersu tem tantas entradas que parece a 470')
     end;
+
+// procedure views_menu_jogada(); // add parametros
+//     begin
+//         writeln(' --------------Placar partida-------------');
+//          writeln('       | USUARIO: 10 | MAQUINA: 6 |');
+//          writeln('  -------------Placar por rodada----------- ');
+//          writeln('   1 - Jogador');
+//          writeln('   2 - Maquina');
+//          writeln('   3 - Empache');
+//          writeln('   ---------Informações da rodada-----------');
+//          writeln('   Manilha: 10');
+//          writeln('   ------------Escolha uma carta------------');
+//          writeln('   [1] - CARTA 1 [2] - CARTA 2 [3] - CARTA 3') ;
+//          writeln('   -------------Outras ações---------------');
+//          writeln('   [4]- pedir truco.');
+//     end;
+
+// procedure views_menu_jogada_resp_truco();// add parametros
+//     begin
+//         writeln(' --------------Placar partida-------------');
+//          writeln('       | USUARIO: 10 | MAQUINA: 6 |');
+//          writeln('  -------------Placar por rodada----------- ');
+//          writeln('   1 - Jogador');
+//          writeln('   2 - Maquina');
+//          writeln('   3 - Empache');
+//          writeln('   ---------Informações da rodada-----------');
+//          writeln('   Manilha: 10');
+//          writeln('   Valor da rodada: 1');
+//          writeln('   ------------Suas  carta------------');
+//          writeln('   CARTA 1 | CARTA 2| CARTA 3') ;
+//          writeln('   -------------Escolha as ações---------------');
+//          if not (valor_rodada < 12) then
+//             writeln('   [5]- Correr. [6]- Aceitar [7]- Aumentar 3');
+//          else 
+//             writeln('  [5]- Correr. [6]- Aceitar')  
+            
+//     end;  
 end.
 
 
