98940f91ef4c436a6d25d75a68fc77758f5a9cbc
Alison Loffi <alison117gh@gmail.com>
2023-04-13 23:38:13 -0300

    Adicionado sistema do jogo anterior, e continuar a logica principal pra testes

--- jogo.pas
+++ jogo.pas
@@ -9,6 +9,7 @@ uses crt,
      pontuacao,
      util;
 var op_main_menu: integer;
+    pontuacao_anterior: tPontuacao;
 
 procedure init();
 begin
@@ -42,58 +43,65 @@ procedure inicio_partida();
 var coringa:integer;
     baralho_lista, mao_usuario,  mao_maquina: tListaCarta;
     baralho_pilha: tPilhaCarta;
-    pontucao: tPontuacao;
+    pontuacao: tPontuacao;
     rodada: tRodada;
+    rodada_ganhador: string;
     i, j:integer;
 begin
     writeln('Inicio da partida.');
 
     baralho_lista := baralho_init();
-    pontuacao_inicializar(pontucao);
+    pontuacao_inicializar(pontuacao);
+
+    // Laço de repetição principal, cada iteração é uma rodada, até alguem vencer o jogo.
+    while (not(pontuacao_tem_vencedor(pontuacao))) do begin
+      // Limpa a tela e motra a pontuacao atual
+      clrscr();
+      pontuacao_mostrar_pontos(pontuacao);
+      writeln('Nova rodada iniciada.');
+
+      // Inicializa o baralho e as mãos dos jogadores
+      rodada_inicializar(rodada);
       baralho_pilha := baralho_embaralha(baralho_lista);
       baralho_realiza_corte(20, baralho_pilha);
       inicializarListaCarta(mao_usuario, 3);
       inicializarListaCarta(mao_maquina, 3);
       baralho_distrubui_cartas(mao_usuario,  mao_maquina, baralho_pilha, coringa);
-      escreverListaCarta(mao_usuario);
-      escreverListaCarta(mao_maquina);
-      writeln(coringa);
+      // escreverListaCarta(mao_usuario);
+      // escreverListaCarta(mao_maquina);
+      // writeln(coringa);
 
-    while (not(pontuacao_tem_vencedor(pontuacao)))
-    rodada_inicializar(rodada);
-    //   While da rodada
-    while ( not(rodada_finalizada(rodada))) do begin
+      // Laço de repetição da rodada (melhor de 3);
+      while ( not(rodada_finalizada(rodada))) do begin
 
-        // ganhador:= joogada_fazer_jogada(rodada_quem_comeca(rodada), mao_usuario, mao_maquina, coringa);
-        i:= aleatorioEntre(1, 3);
-        if (i = 1) then
-            rodada_adicionar_resultado(rodada, 'USUARIO')
-        else if (i = 2) then
-            rodada_adicionar_resultado(rodada, 'MAQUINA')
-        else 
-            rodada_adicionar_resultado(rodada, 'EMPACHE');
+          // ganhador:= joogada_fazer_jogada(rodada_quem_comeca(rodada), mao_usuario, mao_maquina, coringa);
+          i:= aleatorioEntre(1, 3);
+          if (i = 1) then
+              rodada_adicionar_resultado(rodada, 'USUARIO')
+          else if (i = 2) then
+              rodada_adicionar_resultado(rodada, 'MAQUINA')
+          else
+              rodada_adicionar_resultado(rodada, 'EMPACHE');
 
-        rodada_mostrar_resultado(rodada);
-        delay(1000);
+          rodada_mostrar_resultado(rodada);
+          delay(100);
+      end;
+
+      // Rodada finalizada, mostra os resultados e marca na pontuacao global.
+      writeln('Rodada finalizada, resultado: ');
+      rodada_mostrar_resultado(rodada);
+      rodada_ganhador:= rodada_jogador_ganhador(rodada);
+      writeln('Vencedor: ', rodada_ganhador);
+
+      if (rodada_ganhador <> 'NINGUEM') then
+        pontuacao_marcar_pontos(pontuacao, rodada_ganhador, 0);
+      delay(100);
     end;
-    writeln('Rodada finalizada, resultado: ');
-    rodada_mostrar_resultado(rodada);
-    writeln('Vencedor: ', rodada_jogador_ganhador(rodada));
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    clrscr();
+    writeln('JOGO FINALIZADO!');
+    writeln('Ganhador: ', pontuacao_vencedor(pontuacao));
+    pontuacao_mostrar_pontos(pontuacao);
+    pontuacao_anterior:= pontuacao;
 
 end;
 
@@ -110,7 +118,7 @@ Begin
         else if(op_main_menu = 2) then
             views_authors()
         else if(op_main_menu = 3) then
-            Writeln('mostra resultado anterior')
+            pontuacao_mostrar_pontos(pontuacao_anterior)
         else if(op_main_menu = 4) then
             Writeln('Adeus Jogador')
         else if(op_main_menu = 5) then
